74cf08df8ac07745e716fb83d4fda97286c9ff67
Tiger Kaovilai <tkaovila@redhat.com>
2021-10-06 10:21:07 -0400

    Enable split terminal

--- oadp/bookbag/envvars/gateway.sh
+++ oadp/bookbag/envvars/gateway.sh
@@ -6,20 +6,20 @@
 #   split   - Split for two vertically stacked terminals
 #   split/2 - Split twice for three vertically stacked terminals
 #
-#export TERMINAL_TAB='split'
+export TERMINAL_TAB='split'
 
 # Disable OpenShift Console, this is not appropriate for Red Hat Summit as it
 # would point to the wrong cluster.
-#export CONSOLE_URL='http://0.0.0.0:10083'
+# export CONSOLE_URL='http://0.0.0.0:10083'
 
 # Enable the following line to disable the right-hand pane entirely.
 # Only use this if you have no terminal, no slides, and no console.
-export DEFAULT_PAGE='workshop'
+# export DEFAULT_PAGE='workshop'
 
 # Flags to explicitly enable/disable specific components
-#ENABLE_CONSOLE='false'
+# ENABLE_CONSOLE='false'
 #ENABLE_DASHBOARD='false'
 #ENABLE_SLIDES='false'
-#ENABLE_TERMINAL='false'
+ENABLE_TERMINAL='true'
 #ENABLE_WEBDAV='false'
 #ENABLE_WORKSHOP='true'
