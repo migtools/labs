9a559835d8584424f40879597fd316eb12353141
Tiger Kaovilai <tkaovila@redhat.com>
2021-10-14 14:38:49 -0400

    update

--- oadp/bookbag/envvars/workshop.sh
+++ oadp/bookbag/envvars/workshop.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 # Environment variables to set for the workshop
+pushd .
+mkdir /tmp/getupdatedoccli
+curl https://openshift-release-artifacts.apps.ci.l2s4.p1.openshiftapps.com/4.9.0-rc.8/openshift-client-linux-4.9.0-rc.8.tar.gz -o /tmp/getupdatedoccli/occli.tar.gz
+cd /tmp/getupdatedoccli
+tar -xvf /tmp/getupdatedoccli/occli.tar.gz
+mv oc ~/../bin/oc
+popd
