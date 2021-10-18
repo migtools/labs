8b7b24c4943771aca832978013136f6652efa298
Tiger Kaovilai <tkaovila@redhat.com>
2021-10-18 13:36:30 -0400

    Install velero cli

--- oadp/bookbag/envvars/workshop.sh
+++ oadp/bookbag/envvars/workshop.sh
@@ -4,9 +4,16 @@
 pushd .
 # Install desired oc client version, from releases https://amd64.ocp.releases.ci.openshift.org/
 mkdir /tmp/getupdatedocclient
-curl https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz -o /tmp/getupdatedocclient/occli.tar.gz
 cd /tmp/getupdatedocclient
+curl https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz -o /tmp/getupdatedocclient/occli.tar.gz
 tar -xvf /tmp/getupdatedocclient/occli.tar.gz
 mv oc ~/../bin/oc
 mv kubectl ~/../bin/kubectl
+
+# Install velero CLI
+mkdir /tmp/velero-client
+cd /tmp/velero-client
+curl -L https://github.com/vmware-tanzu/velero/releases/download/v1.7.0/velero-v1.7.0-linux-amd64.tar.gz -o /tmp/velero-client/velero-cli.tar.gz
+tar -xvf /tmp/velero-client/velero-cli.tar.gz
+mv velero-v1.7.0-linux-amd64/velero ~/../bin/velero
 popd
