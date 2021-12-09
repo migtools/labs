427bfe62f2d67816f29ad57b69b6f3079efc2bdc
Tiger Kaovilai <tkaovila@redhat.com>
2021-12-09 01:13:33 -0500

    Address feedback, bump to 0.5.1, simplify vars, new local agnosticd deploy script (#45)
    * Velero plugin, cli, api types details.
    
    * bump min requirement ocp version to 4.7 due to missing volumesnapshotclass/v1 in 4.6
    
    * mtc link wording change
    
    * added link to troubleshoting.md
    
    * indent
    
    * intro typo
    
    * expand overview
    
    * addressing NITs
    
    * remove unused vars
    
    * feedback
    
    * simplify vars
    
    * vars fix
    
    * add script to deploy ocs-poc, oadp, and bookbag on ocp4 cluster
    
    * add oc login check
    
    * switch to dpa
    
    * move pip upgrade to after venv
    
    * apiv1 ref update
    
    * newline
    
    * bump oadp version
    
    * add install-deploy instruction
    
    * konveyor changes

--- oadp/README.md
+++ oadp/README.md
@@ -14,6 +14,7 @@ Container Platform 4 leveraging both the UI and API.
 * Tips, techniques, and best-practices for debugging failed backups/restores.
 
 ## Setup Steps
+### Option 1: RHPDS OADP 
 
 Step 1: Browse to RHPDS: https://rhpds.redhat.com
 
@@ -23,11 +24,20 @@ Step 2: Login with your RHPDS username/password.
 
 Step 3: Hover over Services Menu >> Click the Catalogs menu and open the “Openshift Workshop” catalog.
 
-<!-- TODO Create AgnosticD role for OADP -->
-Step 4: Order OpenShift API for Data Protection
+Step 4: Order **APIs For Data Protection (OADP)**
 
-Step 5: Wait for both services to become available.  ```This will take ~60 min.```. When the environment has finished provisioning you will receive an email with confirmation and access details for the labs.
+Step 5: Wait for services to become available.  ```This will take ~60 min.```. When the environment has finished provisioning you will receive an email with confirmation and access details for the labs.
 <!-- TODO replace this image -->
 ![RHPDS-MyServices](screenshots/rhpds-myservices.png)
 
 Step 6. Proceed with Lab by following steps outlined in bookbag documentation.
+
+### Option 2: Use any OpenShift 4.7-4.8 cluster that you have cluster-admin login with kubeadmin-password.
+The cluster should not have existing `openshift-storage` and `openshift-adp` namespace
+
+After `oc login` run the following and fill out any prompts for cluster-admin username and password.
+```bash
+curl https://raw.githubusercontent.com/kaovilai/labs/master/oadp/install-deploy.sh -o install-deploy.sh;
+chmod +x ./install-deploy.sh
+./install-deploy.sh
+```
---
+++ oadp/install-deploy.sh
@@ -0,0 +1,70 @@
+pushd .
+
+if oc whoami > /dev/null 2>&1; then
+  echo "You are logged in to the OpenShift cluster."
+else
+  echo "You are not logged in to the OpenShift cluster."
+  echo "Please log in to the OpenShift cluster and re-run this script."
+  exit 1
+fi
+ANSIBLE_USER=$(whoami)
+
+# if theses are not set, you won't be prompted again
+if [[ -n $CLUSTER_ADMIN_USER ]]; then
+    echo "CLUSTER_ADMIN_USER is set to $CLUSTER_ADMIN_USER"
+else
+    echo "What is cluster admin username? example: kubeadmin or admin"
+    read CLUSTER_ADMIN_USER
+fi
+
+if [[ -n $STUDENT_PASSWORD ]]; then 
+    echo "STUDENT_PASSWORD is set to $STUDENT_PASSWORD"
+else
+    echo "What is cluster admin password? example: XXXXX-XXXXX-XXXXX-XXXXX"
+    read STUDENT_PASSWORD
+fi
+
+RAND_TMP_DIR=/tmp/oadp-lab-day-$RANDOM
+RAND_VENV_DIR=/tmp/oadp-lab-day-$RANDOM-venv
+OCS_NAMESPACE=openshift-storage
+echo "Using temp dir $RAND_TMP_DIR"
+mkdir $RAND_TMP_DIR
+cd $RAND_TMP_DIR
+git clone https://github.com/konveyor/agnosticd --single-branch --branch konveyor-dev
+cd $RAND_TMP_DIR/agnosticd
+
+python3 -m venv $RAND_VENV_DIR
+source $RAND_VENV_DIR/bin/activate
+python3 -m pip install --upgrade pip
+python3 -m pip install openshift ansible jmespath
+
+sed -i=.bak 's/hosts: all/hosts: localhost/g' ./ansible/configs/ocp-workloads/ocp-workload.yml
+sed -i=.bak 's/k8s_facts/kubernetes.core.k8s_info/g' ./ansible/roles/ocp4-workload-ocs-poc/tasks/workload.yml
+sed -i=.bak 's/k8s_facts/kubernetes.core.k8s_info/g' ./ansible/roles/ocp4-workload-ocs-poc/tasks/./pre_workload.yml
+# this playbook can error out if it already has namespace openshift-storage. This most likely mean ocs-poc was already successfully run.. and therefore can proceed to next step
+ansible-playbook -vvv ./ansible/configs/ocp-workloads/ocp-workload.yml \
+-e"ansible_user=${ANSIBLE_USER}" \
+-e"ocp_workload=ocp4-workload-ocs-poc" \
+-e"silent=False" \
+-e"ocs_namespace=${OCS_NAMESPACE}" \
+-e"ACTION=create"
+
+# clear openshift-adp namespace to remove old operator if any
+echo "Please wait for openshift-adp namespace to delete if there are existing. This can take a while"
+oc delete namespace openshift-adp
+
+# install operator
+ansible-playbook ./ansible/configs/ocp-workloads/ocp-workload.yml \
+-e"ansible_user=${ANSIBLE_USER}" \
+-e"ocp_workload=ocp4-workload-oadp" \
+-e"silent=False" \
+-e"ocs_namespace=${OCS_NAMESPACE}" \
+-e"cluster_admin_user=${CLUSTER_ADMIN_USER}" \
+-e"student_password=${STUDENT_PASSWORD}" \
+-e"ocs_migstorage=true" \
+-e"ACTION=create"
+
+rm -rf $RAND_TMP_DIR
+rm -rf $RAND_VENV_DIR
+
+popd
